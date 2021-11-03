ff7cad46a983b946b9e3dfe4d207080787dd6820
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-11-03 16:37:16 -0300

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.clj-holmes/clj-holmes "0.1.11"
+(defproject org.clojars.clj-holmes/clj-holmes "0.2.11"
   :description "Clojure SAST."
   :url "https://github.com/clj-holmes/clj-holmes"
   :scm {:name "git"
