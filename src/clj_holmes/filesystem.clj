d719a9634f62c67e84e80e77427f780a1ee31953
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-11-09 12:04:00 -0300

    remove unused code

--- src/clj_holmes/filesystem.clj
+++ src/clj_holmes/filesystem.clj
@@ -1,7 +1,5 @@
 (ns clj-holmes.filesystem
-  (:require [clojure.data.json :as json]
-            [clojure.string :as string]
-            [clojure.tools.reader.edn :as edn])
+  (:require [clojure.string :as string])
   (:import (java.io File)))
 
 (defn ^:private remove-dot-slash [filename]
@@ -9,11 +7,8 @@
     (string/replace filename #"\./" "")
     filename))
 
-(defn ^:private file? [^File file]
-  (.isFile file))
-
 (defn ^:private clj-file? [^File file]
-  (and (file? file)
+  (and (.isFile file)
        (-> file .toString (string/includes? "project.clj") not)
        (-> file .toString (.endsWith ".clj"))))
 
@@ -24,17 +19,3 @@
          file-seq
          (filter clj-file?)
          (pmap file-sanitize))))
-
-(defn load-rules! [^String directory]
-  (let [reader (comp edn/read-string slurp)]
-    (->> directory
-         File.
-         file-seq
-         (filter file?)
-         (pmap reader))))
-
-(defn save-sarif-report! [sarif-report directory]
-  (let [sarif-output-file (format "%s/report.sarif" directory)]
-    (when sarif-report
-      (->> sarif-report json/write-str (spit sarif-output-file))
-      (println "Sarif report can be find in" sarif-output-file))))
