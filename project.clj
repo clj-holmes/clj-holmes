180deab50426b990bb10275da9630d6008524a2e
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-11-12 12:05:27 -0300

    change bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.clj-holmes/clj-holmes "1.2.11"
+(defproject org.clojars.clj-holmes/clj-holmes "1.0.0"
   :description "Clojure SAST."
   :url "https://github.com/clj-holmes/clj-holmes"
   :scm {:name "git"
