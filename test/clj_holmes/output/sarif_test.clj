067564d8473718995d9b562d73c884a9448c9f5f
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-11-29 18:08:32 -0300

    add sarif test

--- test/clj_holmes/output/sarif_test.clj
+++ test/clj_holmes/output/sarif_test.clj
@@ -1,16 +1,14 @@
 (ns clj-holmes.output.sarif-test
   (:require [clojure.test :refer :all]
+            [clojure.test.check :as test.check]
             [clj-holmes.specs.scan-result :as specs.scan-result]
+            [clojure.test.check.properties :as prop]
             [clj-holmes.specs.sarif :as specs.sarif]
             [clj-holmes.output.sarif :as output.sarif]
-            [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]))
+            [clojure.spec.alpha :as s]))
 
 (deftest output
   (testing "when scan results conforms with the spec."
-    (let [sample (gen/sample (s/gen ::specs.scan-result/scan-results) 10)]
-
-
-      (s/valid? ::specs.sarif/sarif (output.sarif/output sample)))))
-
-(clojure.test.check/)
+    (let [scan-result->sarif-output (prop/for-all [sample (s/gen ::specs.scan-result/scan-results)]
+                                      (s/valid? ::specs.sarif/sarif (output.sarif/output sample)))]
+      (is (:result (test.check/quick-check 10 scan-result->sarif-output))))))
