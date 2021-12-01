a19f8faa82c5b43e3d48660a6f0012bc1518ee9e
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-12-01 14:08:09 -0300

    merge

--- project.clj
+++ project.clj
@@ -36,15 +36,8 @@
                  [org.clojars.clj-holmes/shape-shifter "0.3.6"]
                  [borkdude/edamame "0.0.15"]]
 
-<<<<<<< HEAD
   :profiles {:dev     {:dependencies [[org.clojure/test.check "1.1.0"]]
-=======
-  :profiles {:dev     {:global-vars  {*warn-on-reflection* true
-                                      *unchecked-math*     :warn-on-boxed}
-                       :dependencies [[org.clojure/test.check "1.1.0"]]
->>>>>>> main
                        :plugins      [[lein-shell "0.5.0"]]}
-
              :uberjar {:global-vars {*assert* false}
                        :aot         :all
                        :main        clj-holmes.main
