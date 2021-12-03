f92b17ee03d32609a86a399837928afd7b96e3f1
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-12-03 17:28:28 -0300

    apply suggestions

--- src/clj_holmes/rules/processor/runner.clj
+++ src/clj_holmes/rules/processor/runner.clj
@@ -14,7 +14,13 @@
 (defn ^:private execute-loaded-rule* [forms requires entry]
   (let [check-fn (:check-fn entry)
         condition-fn (:condition-fn entry)]
-    (if-not (nil? check-fn) (let [results (execute-check-fn-in-forms forms requires check-fn) results-with-metadata (add-match-code-as-meta results)] (-> entry (assoc :result (-> results seq boolean condition-fn)) (assoc :findings results-with-metadata))) entry)))
+    (if-not (nil? check-fn)
+      (let [results (execute-check-fn-in-forms forms requires check-fn)
+            results-with-metadata (add-match-code-as-meta results)]
+        (-> entry
+            (assoc :result (-> results seq boolean condition-fn))
+            (assoc :findings results-with-metadata)))
+      entry)))
 
 (defn execute-loaded-rule [loaded-rule forms requires]
   (walk/postwalk (partial execute-loaded-rule* forms requires) loaded-rule))
