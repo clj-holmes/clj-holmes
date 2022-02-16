b3dd6edc11898a2cde12a68fa5bb6dd445667c1a
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-02-15 21:07:32 -0300

    Update project.clj (#56)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.clj-holmes/clj-holmes "1.4.0"
+(defproject org.clojars.clj-holmes/clj-holmes "1.4.1"
   :description "Clojure SAST."
   :url "https://github.com/clj-holmes/clj-holmes"
   :scm {:name "git"
